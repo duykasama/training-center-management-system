86631cea3eb8bf121d37a298e1470213285707af
duykasama <duykasama@gmail.com>
2023-10-12 14:23:56 +0700

    configured environment

--- FAMS.V0.ApiGateWay/Program.cs
+++ FAMS.V0.ApiGateWay/Program.cs
@@ -1,10 +1,14 @@
+using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
+
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
